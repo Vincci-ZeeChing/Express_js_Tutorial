edc11b40a12aa24278a014544c5c268576b31326
Vincci <chingc0606@gmail.com>
2022-09-28 13:13:22 +0800

    Alternative flavor and mind grenade

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 //CommonJS, every file is module (by default)
 // Modules - Encapsulates Code (only share minimum // what we want)
-const sayHI = require('./5-utils')
+
 const name = require('./4-names');
+const sayHI = require('./5-utils')
+// console.log(name)
+// console.log(sayHI)
+// sayHI('susan')
+// sayHI(name.john)
+// sayHI(name.peter)
 
-console.log(name)
-console.log(sayHI)
+const data = require('./6-alternative_flavor')
+// console.log(data)
 
-sayHI('susan')
-sayHI(name.john)
-sayHI(name.peter)
+require('./7-mind-grenade')
+
