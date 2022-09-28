1db6453f751aaf7fe131baee8d2a38446ad64b20
Vincci <chingc0606@gmail.com>
2022-09-28 12:41:39 +0800

    Global

---
+++ 2-global.js
@@ -0,0 +1,13 @@
+//Global - No Windows !!!
+
+// dirname - path to current directory
+// __filename - file name
+// require function to use module (CommonJS)
+// module - info about current module (file)
+// process - info about env where the program is being executed
+
+// print every one second
+console.log(__dirname)
+setInterval(()=>{
+    console.log("Hello world!")
+},1000)
