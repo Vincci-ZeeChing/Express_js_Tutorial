e2a7609941ac2ee5efecbed495b915a7a117a032
Vincci <chingc0606@gmail.com>
2022-09-30 16:59:05 +0800

    express app

--- 2-express-tutorial/final/4-express_app.js
+++ 2-express-tutorial/final/4-express_app.js
@@ -3,10 +3,10 @@ const path = require('path')
 
 const app = express()
 
-//setup static and middleware
+// setup static and middleware
 app.use(express.static('./public'))
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
 })
 
@@ -14,6 +14,6 @@ app.all('*', (req, res) => {
     res.status(404).send('resource not found')
 })
 
-app.listen(5000,()=>{
-    console.log('server is listening on port 5000...')
+app.listen(5000, () => {
+    console.log('server is listening on port 5000....')
 })
